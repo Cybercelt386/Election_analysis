c0b0a11f0e66fda69aba1e170875de48c0beb7fb
Cybercelt386 <111584967+Cybercelt386@users.noreply.github.com>
2022-09-21 15:36:23 -0700

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,13 @@
+
+import csv
+import os
+
+
+# opens election results 
+results = open('election_results.csv', 'r')
+
+# Option to ouput results of file to teriminal
+#print(results.read())
+
+
+results.close
